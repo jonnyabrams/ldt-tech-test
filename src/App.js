66f680dcd08a786a9ed96025ad2c1aeec9ff2fd2
Jonny Abrams <jonnyabrams@hotmail.com>
2022-06-06 09:40:05 +0200

    Stored data locally

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import { bookings } from './bookings'
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        bookings.map(booking => {
+          return <h1>{booking.firstName}</h1>
+        })
+      }
     </div>
   );
 }
